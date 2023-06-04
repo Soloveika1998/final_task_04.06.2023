2c16f6ea19ba78ef32c823e6685d1549ad267c3c
AlexSol <solovejaleksej5@gmail.com>
2023-06-04 14:56:13 +0300

    add block diagram link

--- Program.cs
+++ Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ссылка на блок-схему метода https://drive.google.com/file/d/1PGtPI5Lxk7jsmRg2_0GFyB7Z2L7y2Ax4/view?usp=sharing
