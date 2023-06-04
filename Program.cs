dd23524db13d635140f5cc7439067ee52a68748d
AlexSol <solovejaleksej5@gmail.com>
2023-06-04 15:15:51 +0300

    program complete

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Ссылка на блок-схему метода https://drive.google.com/file/d/1PGtPI5Lxk7jsmRg2_0GFyB7Z2L7y2Ax4/view?usp=sharing
-//Метод заполнения массива строк с клавиатуры (для окончания ввода необходимо ввести пустую строку)
 
+//Метод заполнения массива строк с клавиатуры (для окончания ввода необходимо ввести пустую строку)
 string [] FillArrayString ()
 {
   int count = 0;
@@ -52,9 +52,13 @@ string [] RemoveLargeStringsInArrayStrings (string [] array)
   }
   return returnArray;
 }
+
 //Метод печати массива строк
 void PrintArray (string [] array)
 {
   for (int i = 0; i < array.Length; i++)
         Console.WriteLine("#[{0}] = {1}", i, array[i]);
 }
+
+string [] s = RemoveLargeStringsInArrayStrings(FillArrayString ());
+PrintArray (s);
